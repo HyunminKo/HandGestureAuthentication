57d8b38f1478475e9bfd2df799e94a227d376bbe
HyunminKo <hyunmin.ko4578@gmail.com>
2017-11-15 14:16:42 -0800

    Add Refer link

--- Reference/README.md
+++ Reference/README.md
@@ -2,3 +2,4 @@
 * [Database for hand gesture recognition](http://sun.aei.polsl.pl/~mkawulok/gestures/)
 * [THE 20BN-JESTER DATASET](https://www.twentybn.com/datasets/jester)
 * [CVonline: Image Databases](http://homepages.inf.ed.ac.uk/rbf/CVonline/Imagedbase.htm)
+* [VIVA](http://cvrr.ucsd.edu/vivachallenge/index.php/hands/hand-detection/)
--- src/datapreprocess.py
+++ src/datapreprocess.py
@@ -4,10 +4,15 @@ import os
 fileNames = os.listdir('./30/')
 fileNames.sort()
 for fileName in fileNames:
-	print(fileName)
-img = cv2.imread('./30/00001.jpg')
+    print(fileName)
+img = cv2.imread('./30/00010.jpg')
+grayImg = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+ret,thresh = cv2.threshold(grayImg,127,255,0)
 
-cv2.imshow('image',img)
+image, contours, hierachy = cv2.findContours(thresh, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+image = cv2.drawContours(img, contours, -1, (0,255,0), 3)
+
+cv2.imshow('image', image)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
