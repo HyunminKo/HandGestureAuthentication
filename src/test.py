2965eed10e4ccf4a066f760545b442799a2d6704
HyunminKo <hyunmin.ko4578@gmail.com>
2017-11-18 15:11:24 -0800

    Extract a data list that we will only use

--- src/test.py
+++ src/test.py
@@ -16,8 +16,6 @@ gt_fileNames.sort()
 cv2.namedWindow('image')
 cv2.setMouseCallback('image', on_mouse)
 
-
-        
 i = 0
 for filename in gt_fileNames:
     with open(gt_path+filename,"r") as f:
@@ -29,6 +27,7 @@ for filename in gt_fileNames:
             print(line_arr[1],line_arr[2],line_arr[3],line_arr[4])
             print(img_path+img_fileNames[i])
             img = cv2.imread(img_path+img_fileNames[i])
+            rows, cols = img.shape[:2]
             cv2.rectangle(img,(int(line_arr[1]),int(line_arr[2])),(int(line_arr[1])+int(line_arr[3]),int(line_arr[2])+int(line_arr[4])),(0,255,0),3)
             cv2.imshow("image",img)
             i+=1
