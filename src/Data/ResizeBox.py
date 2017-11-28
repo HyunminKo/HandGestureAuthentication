9d1f300736558d8aa70284159866d482ddf5d52d
HyunminKo <hyunmin.ko4578@gmail.com>
2017-11-28 12:03:33 -0800

    Find index of max Area

--- src/Data/ResizeBox.py
+++ src/Data/ResizeBox.py
@@ -21,29 +21,34 @@ cv2.setMouseCallback('image', on_mouse)
 i = 0
 for filename in gt_fileNames:
     with open(gt_path+filename,"r") as f:
+        lines=[]
         while True:
             line = f.readline()
             if not line: break
-            if not line.startswith('rightHand_driver'): continue
-            line_arr = line.split(" ")
+            if line.startswith('%'): continue
+            lines.append(line)
+        maxIndex = 0
+        areaVal = 0
+        for l in range(len(lines)):
+            line_arr = lines[l].split(' ')
+            if areaVal < float(line_arr[3])*float(line_arr[4]):
+                areaVal = float(line_arr[3])*float(line_arr[4])
+                maxIndex = l
+       
+        line_arr = lines[maxIndex].split(' ')
+        img = cv2.imread(img_path+img_fileNames[i])
+        img = cv2.resize(img,(176,100),interpolation=cv2.INTER_AREA)
+        rows, cols = img.shape[:2]
+        line_arr[1] = round(float(line_arr[1])/3.6363,1)
+        line_arr[2] = round(float(line_arr[2])/4.8,1)
+        line_arr[3] = round(float(line_arr[3])/3.6363,1)
+        line_arr[4] = round(float(line_arr[4])/4.8,1)
 
-            img = cv2.imread(img_path+img_fileNames[i])
-            img = cv2.resize(img,(176,100),interpolation=cv2.INTER_AREA)
-            rows, cols = img.shape[:2]
-            line_arr[1] = round(float(line_arr[1])/3.6363,1)
-            line_arr[2] = round(float(line_arr[2])/4.8,1)
-            line_arr[3] = round(float(line_arr[3])/3.6363,1)
-            line_arr[4] = round(float(line_arr[4])/4.8,1)
-            
-            cv2.rectangle(img,(int(line_arr[1]),int(line_arr[2])),(int(line_arr[1])+int(line_arr[3]),int(line_arr[2])+int(line_arr[4])),(0,255,0),3)
-            cv2.imshow("image",img)
-            i+=1
-            cv2.waitKey(0)
+        cv2.rectangle(img,(int(line_arr[1]),int(line_arr[2])),(int(line_arr[1])+int(line_arr[3]),int(line_arr[2])+int(line_arr[4])),(0,255,0),3)
+        cv2.imshow("image",img)
+        i+=1
+        print(i)
+        cv2.waitKey(0)
             
 
 cv2.destroyAllWindows()
-'''            
-for filename in img_fileNames:
-    img = cv2.imread(img_path+filename)
-    print(filename)
-'''
